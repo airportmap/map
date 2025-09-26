ca87ebb6628f67e886327917f03952dce0d698cd
komed3 <pk97@outlook.de>
2025-09-26 14:46:15 +0200

    add layer getter

--- src/layers/BaseLayer.ts
+++ src/layers/BaseLayer.ts
@@ -4,31 +4,46 @@ import { Layer as LeafletLayer } from 'leaflet';
 
 export abstract class BaseLayer {
 
-    protected options: APMapLayerOptions;
+    protected options: Required< APMapLayerOptions >;
     protected visible: boolean;
     protected leafletLayer: LeafletLayer;
+
+    public get id () : string { return this.options.id }
+    public get name () : string { return this.options.name }
+    public get description () : string | undefined { return this.options.description }
+    public get group () : string | undefined { return this.options.group }
+    public get minZoom () : number { return this.options.minZoom }
+    public get maxZoom () : number { return this.options.maxZoom }
+    public get performanceImpact () : 'low' | 'medium' | 'high' { return this.options.performanceImpact }
+    public get opacity () : number { return this.options.opacity }
+    public get attribution () : string | undefined { return this.options.attribution }
+    public get pane () : string | undefined { return this.options.pane }
 
     constructor ( options: APMapLayerOptions ) {
 
         this.options = this.mergeDefaultOptions( options );
         this.visible = this.options.visible || false;
-        this.leafletLayer = this.createLeafletLayer();
 
+        this.leafletLayer = this.createLeafletLayer();
         this.initEventHandlers();
 
     }
 
-    private mergeDefaultOptions ( options: APMapLayerOptions ) : APMapLayerOptions {
+    private mergeDefaultOptions ( options: APMapLayerOptions ) : Required< APMapLayerOptions > {
 
-        return deepmerge< APMapLayerOptions >( {
+        return deepmerge< Required< APMapLayerOptions > >( {
             id: '',
             name: '',
+            description: undefined,
+            group: undefined,
             visible: false,
             minZoom: 0,
             maxZoom: Infinity,
             performanceImpact: 'low',
             interactive: true,
-            opacity: 1
+            opacity: 1,
+            attribution: undefined,
+            pane: undefined
         }, options );
 
     }
