f6e3cfe0f2ea819aa623ad8f8e414d1980c9a428
komed3 <pk97@outlook.de>
2025-09-26 10:09:03 +0200

    implement base method

--- src/utils/StateStorage.ts
+++ src/utils/StateStorage.ts
@@ -1,3 +1,4 @@
+import { APMapState } from '@airportmap/types';
 import { APMap } from '@map/core/APMap';
 
 export class StateStorage {
@@ -6,7 +7,7 @@ export class StateStorage {
     private enabled: boolean;
     private restoreOnLoad: boolean;
 
-    constructor( private map: APMap ) {
+    constructor ( private map: APMap ) {
 
         const { enabled = false, restoreOnLoad = true, mapId = null } = this.map.opt.stateStorage;
 
@@ -15,5 +16,24 @@ export class StateStorage {
         this.storageKey = `__apmap_state_${ mapId ?? 'default' }`;
 
     }
+
+    public save () : APMapState | undefined {
+
+        if ( ! this.enabled ) return;
+
+        const state: APMapState = {
+            ...this.map.center,
+            zoom: this.map.zoom
+        };
+
+        localStorage.setItem( this.storageKey, JSON.stringify( state ) );
+
+        return state;
+
+    }
+
+    public clear () : void { localStorage.removeItem( this.storageKey ) }
+
+    public export () : string | null { return localStorage.getItem( this.storageKey ) }
 
 }
