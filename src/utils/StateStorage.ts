c5b01aa8cb5ebccf370ecac069979f65aaa53254
komed3 <pk97@outlook.de>
2025-09-26 10:19:23 +0200

    restore state on load

--- src/utils/StateStorage.ts
+++ src/utils/StateStorage.ts
@@ -8,7 +8,6 @@ export class StateStorage {
     private restoreOnLoad: boolean;
 
     public set enable ( is: boolean ) { this.enabled = is }
-    public set restore ( is: boolean ) { this.restoreOnLoad = is }
 
     constructor ( private map: APMap ) {
 
@@ -17,6 +16,8 @@ export class StateStorage {
         this.enabled = enabled;
         this.restoreOnLoad = restoreOnLoad;
         this.storageKey = `__apmap_state_${ mapId ?? 'default' }`;
+
+        if ( this.enabled && this.restoreOnLoad ) this.restore();
 
         this.map.addEventListener( 'position-changed' as APMapEventType, this.save.bind( this ) );
         this.map.addEventListener( 'zoom-changed' as APMapEventType, this.save.bind( this ) );
@@ -35,6 +36,34 @@ export class StateStorage {
         localStorage.setItem( this.storageKey, JSON.stringify( state ) );
 
         return state;
+
+    }
+
+    public restore () : boolean {
+
+        if ( ! this.enabled ) return false;
+
+        const raw = localStorage.getItem( this.storageKey );
+
+        if ( ! raw ) return false;
+
+        try {
+
+            const state: APMapState = JSON.parse( raw );
+
+            if (
+                typeof state.lat === 'number' &&
+                typeof state.lng === 'number' &&
+                typeof state.zoom === 'number'
+            ) {
+
+                this.map.setView( state.lat, state.lng, state.zoom );
+
+            }
+
+            return true;
+
+        } catch { return false }
 
     }
 
