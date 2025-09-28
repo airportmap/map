e21629a4341b1b1b276f7dc88e5d9a011f8f911f
komed3 <pk97@outlook.de>
2025-09-28 20:25:41 +0200

    add Esri imagery tile layer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { APMap } from '@map/core/APMap';
 
 export { CartoTileLayer } from '@map/layers/CartoTileLayer';
+export { EsriImageryTileLayer } from '@map/layers/EsriImageryTileLayer';
 export { OSMTileLayer } from '@map/layers/OSMTileLayer';
 export { TileLayer } from '@map/layers/TileLayer';
---
+++ src/layers/EsriImageryTileLayer.ts
@@ -0,0 +1,21 @@
+import { APMap } from '@map/core/APMap';
+import { TileLayer } from '@map/layers/TileLayer';
+
+export class EsriImageryTileLayer extends TileLayer {
+
+    constructor ( map: APMap ) {
+
+        super( map, {
+            _id: '__tile_layer__esriImagery__',
+            group: '__tile_layer__',
+            name: 'Esri Imagery Tile Layer',
+            url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attribution: '<a href="https://www.esri.com">Esri</a>, Source: i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP',
+            performanceImpact: 'low',
+            minZoom: 0,
+            maxZoom: 19
+        } );
+
+    }
+
+}
