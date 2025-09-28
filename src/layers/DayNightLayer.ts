3a818f1e88f59c5ebf88b5ed5225ed044ae78d6a
komed3 <pk97@outlook.de>
2025-09-28 11:52:10 +0200

    use leaflet terminator to implement day/night boundary

--- src/layers/DayNightLayer.ts
+++ src/layers/DayNightLayer.ts
@@ -3,59 +3,6 @@ import { BaseLayer } from '@map/layers/BaseLayer';
 import deepmerge from 'deepmerge';
 import { LatLngExpression, Polygon } from 'leaflet';
 
-function julian(date: Date): number {
-    return (date.getTime() / 86400000) + 2440587.5;
-}
-
-function GMST(julianDay: number): number {
-    const d = julianDay - 2451545.0;
-    return (18.697374558 + 24.06570982441908 * d) % 24;
-}
-
-function sunEclipticPosition(julianDay: number) {
-    const n = julianDay - 2451545.0;
-    let L = 280.460 + 0.9856474 * n;
-    L %= 360;
-    let g = 357.528 + 0.9856003 * n;
-    g %= 360;
-    const lambda = L + 1.915 * Math.sin(g * Math.PI / 180) +
-        0.02 * Math.sin(2 * g * Math.PI / 180);
-    return { lambda, g };
-}
-
-function eclipticObliquity(julianDay: number): number {
-    const n = julianDay - 2451545.0;
-    const T = n / 36525;
-    return 23.43929111 -
-        T * (46.836769 / 3600
-            - T * (0.0001831 / 3600
-                + T * (0.00200340 / 3600
-                    - T * (0.576e-6 / 3600
-                        - T * 4.34e-8 / 3600))));
-}
-
-function sunEquatorialPosition(lambda: number, eclObliq: number) {
-    const D2R = Math.PI / 180;
-    const R2D = 180 / Math.PI;
-    let alpha = Math.atan(Math.cos(eclObliq * D2R) * Math.tan(lambda * D2R)) * R2D;
-    let delta = Math.asin(Math.sin(eclObliq * D2R) * Math.sin(lambda * D2R)) * R2D;
-    const lQuadrant = Math.floor(lambda / 90) * 90;
-    const raQuadrant = Math.floor(alpha / 90) * 90;
-    alpha = alpha + (lQuadrant - raQuadrant);
-    return { alpha, delta };
-}
-
-function hourAngle(lng: number, sunPos: { alpha: number }, gst: number): number {
-    const lst = gst + lng / 15;
-    return lst * 15 - sunPos.alpha;
-}
-
-function latitude(ha: number, sunPos: { delta: number }): number {
-    const D2R = Math.PI / 180;
-    const R2D = 180 / Math.PI;
-    return Math.atan(-Math.cos(ha * D2R) / Math.tan(sunPos.delta * D2R)) * R2D;
-}
-
 export class DayNightLayer extends BaseLayer< APMapDayNightLayerOptions > {
 
     private animationFrame: number | null = null;
@@ -70,6 +17,7 @@ export class DayNightLayer extends BaseLayer< APMapDayNightLayerOptions > {
             animationSpeed: 1,
             nightColor: '#000',
             nightOpacity: 0.3,
+            longitudeRange: 720,
             resolution: 36,
             interactive: false
         }, options ) );
@@ -78,31 +26,109 @@ export class DayNightLayer extends BaseLayer< APMapDayNightLayerOptions > {
 
     }
 
+    private julian ( date: Date ) : number {
+
+        return ( date.getTime() / 86400000 ) + 2440587.5;
+
+    }
+
+    private GMST ( julianDay: number ) : number {
+
+        return ( 18.697374558 + 24.06570982441908 * ( julianDay - 2451545.0 ) ) % 24;
+
+    }
+
+    private sunEclipticPosition ( julianDay: number ) : { lambda: number, g: number } {
+
+        const n = julianDay - 2451545.0;
+        const L = ( 280.460 + 0.9856474 * n ) % 360;
+        const g = ( 357.528 + 0.9856003 * n ) % 360;
+
+        const lambda = L + 1.915 * Math.sin( g * Math.PI / 180 ) + 0.02 * Math.sin( 2 * g * Math.PI / 180 );
+
+        return { lambda, g };
+
+    }
+
+    private eclipticObliquity ( julianDay: number ) : number {
+
+        const T = ( julianDay - 2451545.0 ) / 36525;
+
+        return 23.43929111 - T * (
+            46.836769 / 3600 - T * (
+                0.0001831 / 3600 + T * (
+                    0.00200340 / 3600 - T * (
+                        0.576e-6 / 3600 - T * 4.34e-8 / 3600
+                    )
+                )
+            )
+        );
+
+    }
+
+    private sunEquatorialPosition ( lambda: number, eclObliq: number ) : { alpha: number, delta: number } {
+
+        const D2R = Math.PI / 180;
+        const R2D = 180 / Math.PI;
+
+        let alpha = Math.atan( Math.cos( eclObliq * D2R ) * Math.tan( lambda * D2R ) ) * R2D;
+        let delta = Math.asin( Math.sin( eclObliq * D2R ) * Math.sin( lambda * D2R ) ) * R2D;
+
+        alpha = alpha + ( ( Math.floor( lambda / 90 ) * 90 ) - ( Math.floor( alpha / 90 ) * 90 ) );
+
+        return { alpha, delta };
+
+    }
+
+    private hourAngle ( lng: number, sunPos: { alpha: number }, gst: number ) : number {
+
+        return ( gst + lng / 15 ) * 15 - sunPos.alpha;
+
+    }
+
+    private latitude ( ha: number, sunPos: { delta: number } ) : number {
+
+        const D2R = Math.PI / 180;
+        const R2D = 180 / Math.PI;
+
+        return Math.atan( -Math.cos( ha * D2R ) / Math.tan( sunPos.delta * D2R ) ) * R2D;
+
+    }
+
     private calculateDayNightBoundary ( date: Date = new Date() ) : LatLngExpression[] {
 
-        const longitudeRange = 720;
-        const resolution = this.options.resolution || 2;
-        const today = date;
-        const julianDay = julian(today);
-        const gst = GMST(julianDay);
+        const { longitudeRange, resolution } = this.options;
 
-        const sunEclPos = sunEclipticPosition(julianDay);
-        const eclObliq = eclipticObliquity(julianDay);
-        const sunEqPos = sunEquatorialPosition(sunEclPos.lambda, eclObliq);
+        const julianDay = this.julian( date );
+        const gst = this.GMST( julianDay );
+
+        const sunEclPos = this.sunEclipticPosition( julianDay );
+        const eclObliq = this.eclipticObliquity( julianDay );
+        const sunEqPos = this.sunEquatorialPosition( sunEclPos.lambda, eclObliq );
 
         const latLng: LatLngExpression[] = [];
-        for (let i = 0; i <= longitudeRange * resolution; i++) {
+
+        for ( let i = 0; i <= longitudeRange * resolution; i++ ) {
+
             const lng = -longitudeRange / 2 + i / resolution;
-            const ha = hourAngle(lng, sunEqPos, gst);
-            latLng[i + 1] = [latitude(ha, sunEqPos), lng];
+            const ha = this.hourAngle( lng, sunEqPos, gst );
+
+            latLng[ i + 1 ] = [ this.latitude( ha, sunEqPos ), lng ];
+
         }
-        if (sunEqPos.delta < 0) {
-            latLng[0] = [90, -longitudeRange / 2];
-            latLng[latLng.length] = [90, longitudeRange / 2];
+
+        if ( sunEqPos.delta < 0 ) {
+
+            latLng[ 0 ] = [ 90, -longitudeRange / 2 ];
+            latLng[ latLng.length ] = [ 90, longitudeRange / 2 ];
+
         } else {
-            latLng[0] = [-90, -longitudeRange / 2];
-            latLng[latLng.length] = [-90, longitudeRange / 2];
+
+            latLng[ 0 ] = [ -90, -longitudeRange / 2 ];
+            latLng[ latLng.length ] = [ -90, longitudeRange / 2 ];
+
         }
+
         return latLng;
 
     }
