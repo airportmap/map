e34cb1edf4a7155d254e9a5ed364440b7e4cb43f
komed3 <pk97@outlook.de>
2025-09-30 01:18:27 +0200

    implement unit systems

--- src/core/GeoMeasurement.ts
+++ src/core/GeoMeasurement.ts
@@ -1,9 +1,81 @@
+import { APMapUnitSystems, APMapUnitSystemType, APMapUnitSystem } from '@airportmap/types';
 import { APMap } from '@map/core/APMap';
 import { LatLng } from 'leaflet';
 
 export class GeoMeasurement {
 
+    private static readonly UNIT_SYSTEMS: Record< APMapUnitSystems, APMapUnitSystem > = {
+        metric: {
+            distance: {
+                base: 'm',
+                convert: { imperial: 3.2808399, avionic: 3.2808399 },
+                units: { m: 1, km: 0.001 }
+            },
+            speed: {
+                base: 'km/h',
+                convert: { imperial: 0.62137119, avionic: 0.539957 },
+                units: { 'km/h': 1 }
+            },
+            altitude: {
+                base: 'm',
+                convert: { imperial: 3.2808399, avionic: 3.2808399 },
+                units: { 'm': 1 }
+            }
+        },
+        imperial: {
+            distance: {
+                base: 'ft',
+                convert: { metric: 0.3048, avionic: 1 },
+                units: { ft: 1, mi: 0.00018939 }
+            },
+            speed: {
+                base: 'mph',
+                convert: { metric: 1.609344, avionic: 0.86897624 },
+                units: { mph: 1 }
+            },
+            altitude: {
+                base: 'ft',
+                convert: { metric: 0.3048, avionic: 1 },
+                units: { ft: 1 }
+            }
+        },
+        avionic: {
+            distance: {
+                base: 'ft',
+                convert: { metric: 0.3048, imperial: 1 },
+                units: { ft: 1, nm: 0.00016458 }
+            },
+            speed: {
+                base: 'kn',
+                convert: { metric: 1.852, imperial: 1.15077945 },
+                units: { kn: 1 }
+            },
+            altitude: {
+                base: 'ft',
+                convert: { metric: 0.3048, imperial: 1 },
+                units: { ft: 1 }
+            }
+        }
+    };
+
     constructor ( private map: APMap ) {}
+
+    private get effectiveUnits () : APMapUnitSystems {
+
+        const units = this.map.opt.units;
+        return this.map.opt.units === 'default' ? 'metric' : units as APMapUnitSystems;
+
+    }
+
+    private unitConverter (
+        value: number, type: APMapUnitSystemType,
+        from: APMapUnitSystems, to: APMapUnitSystems,
+        unit: string | boolean = true, precision: number = 2
+    ) : number {
+
+        //
+
+    }
 
     private degToDMS ( deg: number, isLat: boolean, precision: number = 2 ) : string {
 
