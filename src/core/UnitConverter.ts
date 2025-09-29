1ec2cb0fde7493438f6b3414f9907e435466be18
komed3 <pk97@outlook.de>
2025-09-29 13:39:46 +0200

    dist. formatter + deg <-> dms

--- src/core/UnitConverter.ts
+++ src/core/UnitConverter.ts
@@ -1,10 +1,8 @@
+import { APMapUnits } from '@airportmap/types';
 import { APMap } from '@map/core/APMap';
+import { LatLng } from 'leaflet';
 
 export class UnitConverter {
-
-    private readonly DEG_SYMBOL = '°';
-    private readonly MIN_SYMBOL = '′';
-    private readonly SEC_SYMBOL = '″';
 
     constructor( private map: APMap ) {}
 
@@ -17,7 +15,7 @@ export class UnitConverter {
 
         const dir = isLat ? ( deg >= 0 ? 'N' : 'S' ) : ( deg >= 0 ? 'E' : 'W' );
 
-        return `${d}${ this.DEG_SYMBOL } ${m}${ this.MIN_SYMBOL } ${s}${ this.SEC_SYMBOL } ${dir}`;
+        return `${d}° ${m}′ ${s}″ ${dir}`;
 
     }
 
@@ -33,6 +31,75 @@ export class UnitConverter {
         if ( dir === 'S' || dir === 'W' ) deg *= -1;
 
         return deg;
+
+    }
+
+    private roundNice ( val: number ) : number {
+
+        if ( val >= 100 ) return Math.round( val / 10 ) * 10;
+        if ( val >=  10 ) return Math.round( val );
+        if ( val >=   1 ) return Math.round( val * 10 ) / 10;
+
+        return Math.round( val * 100 ) / 100;
+
+    }
+
+    private niceScale ( meters: number ) : number {
+
+        const scales = [ 1, 2, 5 ];
+        let exp = Math.floor( Math.log10( meters ) );
+        let base = Math.pow( 10, exp );
+
+        for ( let s of scales ) if ( meters <= s * base ) return s * base;
+        return 10 * base;
+
+    }
+
+    public latLngToHuman ( coords: LatLng ) : { lat: string, lng: string } {
+
+        return {
+            lat: this.degToDMS( coords.lat, true ),
+            lng: this.degToDMS( coords.lng, false )
+        };
+
+    }
+
+    public humanToLatLng ( lat: string, lng: string ) : LatLng {
+
+        return new LatLng(
+            this.dmsToDeg( lat ),
+            this.dmsToDeg( lng )
+        );
+
+    }
+
+    public formatDistance ( distance: number, options: {
+        precision?: number,
+        nice?: boolean,
+        avionic?: boolean,
+        force?: APMapUnits
+    } = {} ) : string {
+
+        let unit = 'km';
+
+        if ( options.avionic ) {
+
+            distance *= 0.539957;
+            unit = 'nm';
+
+        }
+
+        if ( ( options.force ?? this.map.opt.units ) === 'imperial' ) {
+
+            distance *= 0.621371;
+            unit = 'mi';
+
+        }
+
+        return `${ options.nice
+            ? this.roundNice( distance )
+            : distance.toFixed( options.precision ?? 2 )
+        } ${unit}`;
 
     }
 
