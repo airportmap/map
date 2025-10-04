2e81472fb2fa888f4b0da913027762e21f3b3437
komed3 <pk97@outlook.de>
2025-10-04 18:29:04 +0200

    min fixes

--- src/core/HeadingIndicator.ts
+++ src/core/HeadingIndicator.ts
@@ -94,32 +94,32 @@ export class HeadingIndicator {
 
     }
 
-    private animate () : void {
+    private handleAnimate ( which: 'hdg' | 'center' ) : boolean {
 
         const { speed } = this.options;
+        const diff = this.getShortestRotation( this.currentState[ which ], this.targetState[ which ] );
 
-        const handleAnimate = ( which: 'hdg' | 'center' ) : boolean => {
+        if ( Math.abs( diff ) > 0.01 ) {
 
-            const diff = this.getShortestRotation( this.currentState[ which ], this.targetState[ which ] );
-
-            if ( Math.abs( diff ) > 0.01 ) {
-
-                this.currentState[ which ] = this.normalize( this.currentState[ which ] + diff * speed );
-                return true;
-
-            }
-
-            this.currentState[ which ] = this.targetState[ which ];
-            return false;
+            this.currentState[ which ] = this.normalize( this.currentState[ which ] + diff * speed );
+            return true;
 
         }
+
+        this.currentState[ which ] = this.targetState[ which ];
+        return false;
+
+    }
+
+    private animate () : void {
+
+        const needsHdgUpdate = this.handleAnimate( 'hdg' );
+        const needsCenterUpdate = this.handleAnimate( 'center' );
 
         this.updateVisuals();
         this.animationId = null;
 
-        if ( this.container.classList.contains( 'hidden' ) === false && (
-            handleAnimate( 'hdg' ) || handleAnimate( 'center' )
-        ) ) {
+        if ( this.container.classList.contains( 'hidden' ) === false && ( needsHdgUpdate || needsCenterUpdate ) ) {
 
             this.animationId = requestAnimationFrame( this.animate.bind( this ) );
 
